7556d193a345e666a432d1c3b5b427f37455d359
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-09-09 14:12:35 -0500

    Complete initial boilerplate for components

---
+++ client/components/ProductOverview.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductOverview(props) {
+  return (
+    <>
+    <h1>Product Overview</h1>
+    </>
+  )
+}
---
+++ client/components/QuestionsAndAnswers.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function QuestionsAndAnswers(props) {
+  return (
+    <>
+    <h1>Questions And Answers</h1>
+    </>
+  )
+}
