9d6bac309e8767fb624191073ad5a8db8d3c6142
aliciav-texas <aliciav.texas@gmail.com>
2021-09-10 13:29:11 -0500

    "Updated server index.js, created a models file to hold ll methods. Be sure to replace your config.example.js to config.js and lastly add it to git ignore! That change should be there on this PR"

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
-var express =  require('express');
+const express = require('express');
+const config = require('./config.js');
+const PORT = 3030;
+const axios = require('axios');
+const models = require('./models.js');
 
-var PORT = 3030;
-var app = express();
+let app = express();
 
 app.use(express.static('dist'));
+app.use(express.json());
+
+app.get('/products', (req, res) => {
+  //request body will need to have the id for API query
+  return models.getAllProducts()
+    .then((results) => {
+      console.log('results from index.js', results);
+    })
+    .catch((error) => {
+      console.log('you hit an error on server/index.js')
+    })
+})
+
 
 app.listen(PORT, (err, success) => {
   if (err) {
