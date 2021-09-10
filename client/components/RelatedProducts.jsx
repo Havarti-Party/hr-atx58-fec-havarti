c2b83f855646eb29cd41b2cc0f4d45969f7b5021
aliciav-texas <aliciav.texas@gmail.com>
2021-09-09 20:09:15 -0500

    "Created a practce carousel with old React files. Added in package.json because I installed a React package. I did not add in main.js but it appears changes were made."

--- client/components/RelatedProducts.jsx
+++ client/components/RelatedProducts.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import RelatedProductCard from './RelatedProductCard.jsx'
 
 export default function RelatedProducts(props) {
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+
+
+
+  let testArr = [{
+    imgSrc: "../Images/stock.jpg",
+    sampleText: 'Image 1',
+  }, {
+    imgSrc: "../Images/stock.jpg",
+    sampleText: 'Image 2',
+  }, {
+    imgSrc: "../Images/stock.jpg",
+    sampleText: 'Image 3',
+  }, {
+    imgSrc: "../Images/stock.jpg",
+    sampleText: 'Image 4',
+  }, {
+    imgSrc: "../Images/stock.jpg",
+    sampleText: 'Image 5',
+  }]
+
   return (
     <>
-    <h1>Related Products</h1>
+      <Carousel responsive={responsive}>
+        {testArr.map((obj) => {
+          return <RelatedProductCard imgObj={obj} />
+        })}
+      </Carousel>
     </>
   )
 }
