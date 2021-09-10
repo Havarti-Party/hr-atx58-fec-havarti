610b4d73c481f062345847d18a828b672b14afb9
travis503 <travis503@gmail.com>
2021-09-09 19:27:33 -0500

    Add REAL basic mockup

--- client/components/RatingsAndReviews.jsx
+++ client/components/RatingsAndReviews.jsx
@@ -3,7 +3,53 @@ import React from 'react';
 export default function RatingsAndReviews(props) {
   return (
     <>
-    <h1>Ratings And Reviews</h1>
+    <h3>Ratings And Reviews</h3>
+    <div>100% of reviews recommend this product</div>
+    <RatingBreakdownBars />
+    <SizeBar />
+    <ComfortBar />
+    <ReviewSorter />
+    <ReviewDisplay />
     </>
+  )
+}
+
+var RatingBreakdownBars = (props) => {
+  return (
+    <div>This is where the breakdown bars go</div>
+  )
+}
+
+var SizeBar = (props) => {
+  return (
+    <div>This is where the size bar goes</div>
+  )
+}
+
+var ComfortBar = (props) => {
+  return (
+    <div>This is where the comfort bar goes</div>
+  )
+}
+
+var ReviewSorter = (props) => {
+  return (
+    <div>This is where the review sorter goes</div>
+  )
+}
+
+var ReviewDisplay = (props) => {
+  return (
+    <>
+    <div>This is where the review display goes</div>
+    <ReviewTile />
+    <ReviewTile />
+    </>
+  )
+}
+
+var ReviewTile = (props) => {
+  return (
+    <div>This is a review tile</div>
   )
 }
