7ddfaad3388042cd37b61793517f23e69d2b9076
aliciav-texas <aliciav.texas@gmail.com>
2021-09-13 10:41:38 -0500

    "Imported new features and changed format of cards"

--- client/components/RelatedProductCard.jsx
+++ client/components/RelatedProductCard.jsx
@@ -1,21 +1,58 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import StarRatings from 'react-star-ratings';
 
-const RelatedProductCard = ({ imgObj }) => {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 300,
+  },
+  media: {
+    height: 250,
+  },
+});
+
+export default function RelatedProductCard({ imgObj }) {
+  const classes = useStyles();
 
   return (
-    <div className="card">
-      <h5> {imgObj.sampleText} </h5>
-      <img className="stock" src={imgObj.imgSrc} />
-      <StarRatings
-        rating={2}
-        starDimension={'15px'}
-        starSpacing={'1px'}
-      />
-      <h5>Description will go here </h5>
-    </div>
-  )
-
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={imgObj.imgSrc}
+          title={imgObj.sampleText}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Product Title
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            descriptive text from RelatedProducts obj
+            Price
+          </Typography>
+          <StarRatings
+            rating={2}
+            starDimension={'15px'}
+            starSpacing={'1px'}
+          />
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Add To Outfit
+        </Button>
+        <Button size="small" color="primary">
+          Compare to Overview
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
 
-export default RelatedProductCard;
--- client/components/RelatedProducts.jsx
+++ client/components/RelatedProducts.jsx
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import RelatedProductCard from './RelatedProductCard.jsx'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 export default function RelatedProducts(props) {
 
