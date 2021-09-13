51a9c96aaa411f35115cfd6e1626eb49e8eed04f
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-09-13 08:30:06 -0500

    Add star rating component to Product overview

--- client/components/ProductOverview.jsx
+++ client/components/ProductOverview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import MainImageCarousel from './MainImageCarousel.jsx';
+import ProductOverviewStarRating from './ProductOverviewStarRating.jsx';
 
 export default function ProductOverview(props) {
   return (
@@ -11,7 +12,7 @@ export default function ProductOverview(props) {
           <img src="../images/description.png" alt="Description 1" width="100%" />
       </Grid>
       <Grid item md={4}>
-          <img src="../images/stars.png" alt="Star Rating" width="70%" />
+          <ProductOverviewStarRating /><a href="" style={{fontSize: '12px'}} >Read All Reviews</a>
           <img src="../images/productDetails.png" alt="Product Details" width="90%" />
           <img src="../images/styles.png" alt="Styles" width="100%" />
           <img src="../images/addToCart.png" alt="Add to cart" width="100%" />
