6d85ef20283407c3ce3cc4c7c76a5c8e0bd54206
Johnathan Brennan <jbrennan4306@gmail.com>
2021-09-14 02:23:02 -0500

    hardcoded components with sample data

--- client/components/QuestionsAndAnswers.jsx
+++ client/components/QuestionsAndAnswers.jsx
@@ -1,28 +1,70 @@
-import React from 'react';
-import Question from './Question.jsx'
-
-class QuestionsAndAnswers extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      questions: [1, 2, 3, 4],
-      answers: [],
-    }
-
-  }
+import React, { useState } from 'react';
+import Question from './Question.jsx';
+import Button from '@material-ui/core/Button';
 
 
-  render() {
-    return (
-      <div id='questionList'>
-        <h1>Questions And Answers</h1>
-        {this.state.questions.map(item => {
-          return <Question />
-        })}
-      </div>
-    )
-  }
+export default function QuestionsAndAnswers(props) {
+  const [questions, setQuestions] = useState(() => sampleQuestions)
+
+  return (
+    <div id='questionList'>
+      <h1>Customer Questions And Answers</h1>
+      <input type='text' placeholder='search for a specific question here'/>
+      {questions.map(question => {
+        return <Question question={question}/>
+      })}
+      <Button variant='contained'>expand answers</Button>
+      <Button variant='contained'>add a question</Button>
+    </div>
+  )
 }
 
 
-export default QuestionsAndAnswers
+const sampleQuestions = [
+    {
+    "question_id": 37,
+    "question_body": "Why is this product cheaper here than other sites?",
+    "question_date": "2018-10-18T00:00:00.000Z",
+    "asker_name": "williamsmith",
+    "question_helpfulness": 4,
+    "reported": false,
+    "answers": {
+      68: {
+        "id": 68,
+        "body": "We are selling it here without any markup from the middleman!",
+        "date": "2018-08-18T00:00:00.000Z",
+        "answerer_name": "Seller",
+        "helpfulness": 4,
+        "photos": []
+        // ...
+      }
+    }
+  },
+  {
+    "question_id": 38,
+    "question_body": "How long does it last?",
+    "question_date": "2019-06-28T00:00:00.000Z",
+    "asker_name": "funnygirl",
+    "question_helpfulness": 2,
+    "reported": false,
+    "answers": {
+      70: {
+        "id": 70,
+        "body": "Some of the seams started splitting the first time I wore it!",
+        "date": "2019-11-28T00:00:00.000Z",
+        "answerer_name": "sillyguy",
+        "helpfulness": 6,
+        "photos": [],
+      },
+      78: {
+        "id": 78,
+        "body": "9 lives",
+        "date": "2019-11-12T00:00:00.000Z",
+        "answerer_name": "iluvdogz",
+        "helpfulness": 31,
+        "photos": [],
+      }
+    }
+  },
+  //...
+]
