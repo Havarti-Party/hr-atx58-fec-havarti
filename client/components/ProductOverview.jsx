268e9e89450c819e8c614a359935127f42eb0e3f
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-09-14 16:52:34 -0500

    Commented out AddToCart to quickfix error

--- client/components/ProductOverview.jsx
+++ client/components/ProductOverview.jsx
@@ -130,9 +130,9 @@ export default function ProductOverview(props) {
           selectedStyle={selectedStyle}
           handleStyleClick={handleStyleClick}
         />
-        <AddToCart
+        {/* <AddToCart
           skus={selectedStyle.skus}
-        />
+        /> */}
       </Grid>
     </Grid>
     <Grid container>
