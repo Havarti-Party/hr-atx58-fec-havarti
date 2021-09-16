cc08d59d3d3d07bae6572ea083e50c545fdd7fb0
aliciav-texas <aliciav.texas@gmail.com>
2021-09-16 11:58:09 -0500

    "refactored to make overviewProduct an object and not at array"

--- client/components/ProductsContext.jsx
+++ client/components/ProductsContext.jsx
@@ -1,37 +1,33 @@
-import React, { useState, createContext } from "react";
+import React, { useEffect, useRef, useState, createContext } from "react";
+import axios from "axios";
 
 export const ProductsContext = createContext();
 
 export const ProductsProvider = (props) => {
-  const [overviewProduct, setOverviewProduct] = useState([
-    {
-      id: 38329,
-      campus: "hr-atx",
-      name: "YEasy 350",
-      slogan: "Just jumped over jumpman",
-      description:
-        "These stretchy knit shoes show off asymmetrical lacing and a big sculpted rubber midsole. In a nod to adidas soccer heritage.",
-      category: "Kicks",
-      default_price: "450.00",
-      created_at: "2021-08-13T14:38:00.907Z",
-      updated_at: "2021-08-13T14:38:00.907Z",
-      url: "https://images.unsplash.com/photo-1551489186-cf8726f514f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-      features: [
-        {
-          feature: "Sole",
-          value: "Rubber",
-        },
-        {
-          feature: "Material",
-          value: "FullControlSkin",
-        },
-        {
-          feature: "Stitching",
-          value: "Double Stitch",
-        },
-      ],
-    },
-  ]);
+  const [products, setProducts] = useState([]);
+
+  const [overviewProduct, setOverviewProduct] = useState();
+
+  const isMounted = useRef(false);
+  useEffect(() => {
+    axios
+      .get("/products")
+      .then((productsArray) => {
+        setProducts(productsArray.data);
+      })
+      .catch((error) => {
+        console.log("error, unable to get productsArray from localhost server");
+      });
+  }, []);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log("products has changed", products);
+      setOverviewProduct(products[0]);
+    } else {
+      isMounted.current = true;
+    }
+  }, [products]);
 
   return (
     <ProductsContext.Provider value={[overviewProduct, setOverviewProduct]}>
