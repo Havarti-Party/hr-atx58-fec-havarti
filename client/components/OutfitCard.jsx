f11d70e0b3a52e766b714eac6e89bbd6243112ce
aliciav-texas <aliciav.texas@gmail.com>
2021-09-21 11:53:49 -0500

    "Moved the X icon to the right'

--- client/components/OutfitCard.jsx
+++ client/components/OutfitCard.jsx
@@ -12,7 +12,8 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import StarRatings from "react-star-ratings";
 
-//Modal
+//Grid
+import Grid from "@material-ui/core/Grid";
 
 //Icons
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
@@ -47,11 +48,15 @@ export default function OutfitCard({ OutfitObj, remove }) {
         image={OutfitObj.url ? OutfitObj.url : noImage}
         title={OutfitObj.name}
       >
-        <HighlightOffIcon
-          onClick={() => {
-            remove(OutfitObj);
-          }}
-        />
+        <Grid container direction="column" alignItems="flex-end">
+          <Grid item>
+            <HighlightOffIcon
+              onClick={() => {
+                remove(OutfitObj);
+              }}
+            />
+          </Grid>
+        </Grid>
       </CardMedia>
       <CardActionArea
         onClick={() => {
