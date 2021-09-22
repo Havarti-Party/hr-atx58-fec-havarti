1330a34349f5a8272a0560d6c237656ac6e25611
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-09-22 17:19:39 -0500

    Fix conflict

--- client/components/ProductsContext.jsx
+++ client/components/ProductsContext.jsx
@@ -1,6 +1,6 @@
+/* eslint-disable react/prop-types */
 import React, { useEffect, useRef, useState, createContext } from "react";
 import axios from "axios";
-import PropTypes from 'prop-types';
 
 export const ProductsContext = createContext();
 
@@ -20,7 +20,7 @@ export const ProductsProvider = (props) => {
         setProducts(productsArray.data);
       })
       .catch(() => {
-        console.error("error, unable to get productsArray from localhost server");
+        console.log("error, unable to get productsArray from localhost server");
       });
   }, []);
 
@@ -29,56 +29,61 @@ export const ProductsProvider = (props) => {
       let overviewProductID = products[0].id;
       Promise.all([
         axios.get("/related", {
-            params: {
-              ID: overviewProductID,
-        }}),
+          params: {
+            ID: overviewProductID,
+          },
+        }),
         axios.get("/styles", {
           params: {
             ID: overviewProductID,
-        }})
+          },
+        }),
       ])
-      .then(([overviewProductDetails, overviewProductStyles]) => {
-        products[0].features = overviewProductDetails.data.features;
-        products[0].url = overviewProductDetails.data.url;
-        setStyles(overviewProductStyles.data.results)
-        setOverviewProduct(products[0]);
-      })
-      .catch(() => {
-        console.error("could not get styles for overview product");
-      });
+        .then(([overviewProductDetails, overviewProductStyles]) => {
+          products[0].features = overviewProductDetails.data.features;
+          products[0].url = overviewProductDetails.data.url;
+          setStyles(overviewProductStyles.data.results);
+          setOverviewProduct(products[0]);
+        })
+        .catch(() => {
+          console.log("could not get styles for overview product");
+        });
     }
   }, [products]);
 
   useEffect(() => {
     if (isMounted.current) {
       let overviewProductID = overviewProduct.id;
-      axios.get("/styles", {
-        params: {
-          ID: overviewProductID,
-      }})
-      .then((overviewProductStyles) => {
-
-        setStyles(overviewProductStyles.data.results)
-        setSelectedStyle(overviewProductStyles.data.results[0])
-        setIsLoading(false);
-      })
-      .catch(() => {
-        console.error("could not get styles for overview product");
-      });
+      axios
+        .get("/styles", {
+          params: {
+            ID: overviewProductID,
+          },
+        })
+        .then((overviewProductStyles) => {
+          setStyles(overviewProductStyles.data.results);
+          setSelectedStyle(overviewProductStyles.data.results[0]);
+          setIsLoading(false);
+        })
+        .catch(() => {
+          console.log("could not get styles for overview product");
+        });
     } else {
       isMounted.current = true;
     }
   }, [overviewProduct]);
 
-  const children = props.children
-
   return (
-    <ProductsContext.Provider value={{ overviewProduct: [overviewProduct, setOverviewProduct], isLoading: [isLoading, setIsLoading], starRating: [starRating, setStarRating], stylesState: [styles, setStyles], selectedStyleState: [selectedStyle, setSelectedStyle]}}>
-      {children}
+    <ProductsContext.Provider
+      value={{
+        overviewProduct: [overviewProduct, setOverviewProduct],
+        isLoading: [isLoading, setIsLoading],
+        starRating: [starRating, setStarRating],
+        stylesState: [styles, setStyles],
+        selectedStyleState: [selectedStyle, setSelectedStyle],
+      }}
+    >
+      {props.children}
     </ProductsContext.Provider>
   );
 };
-
-ProductsContext.propTypes = {
-  children: PropTypes.element.isRequired
-}
