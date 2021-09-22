6dce2487db37ea4b3b8bdeda35edf5d9238a0575
travis503 <travis503@gmail.com>
2021-09-21 19:58:10 -0500

    Implements reactive review factor bars

--- client/components/RatingsAndReviews.jsx
+++ client/components/RatingsAndReviews.jsx
@@ -29,7 +29,7 @@ export default function RatingsAndReviews(props) {
         }
       })
       .then((reviewData) => {
-        console.log('Review data object: ', reviewData.data);
+        // console.log('Review data object: ', reviewData.data);
         setCurrentReviews(reviewData.data);
         setLoading(false);
       })
