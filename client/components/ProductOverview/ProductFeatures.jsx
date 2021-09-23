9166f01bba57ae4aa565f8c829759eedf7cdefba
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2021-09-23 11:36:07 -0500

    Minor visual changes

--- client/components/ProductOverview/ProductFeatures.jsx
+++ client/components/ProductOverview/ProductFeatures.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-
+import Grid from "@material-ui/core/Grid";
 
 export default function ProductFeatures({features}) {
 
   return (
     <>
-    Product features
-    <ul>
-      {features.map((feature, i) => (
-        <li key={i}>{feature.feature}: {feature.value}</li>
-      ))}
-    </ul>
+      <Grid item xs={12}>
+        <h3>Product features</h3>
+      </Grid>
+      <Grid item xs={12}>
+        <ul>
+          {features.map((feature, i) => (
+            <li key={i}>{feature.feature}: {feature.value}</li>
+          ))}
+        </ul>
+      </Grid>
     </>
-
   )
 }
