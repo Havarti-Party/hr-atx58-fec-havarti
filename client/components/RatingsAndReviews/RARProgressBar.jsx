12d26eddc17bc5aff454ce8443f95f7f5842b227
travis503 <travis503@gmail.com>
2021-09-24 10:13:24 -0500

    Swaps progress bars to sliders

--- client/components/RatingsAndReviews/RARProgressBar.jsx
+++ client/components/RatingsAndReviews/RARProgressBar.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Slider from '@material-ui/core/Slider';
 
 
 export default function ProgressBar(props) {
+
+  var starSort = () => {
+    console.log('Clicked!');
+  }
+
   return (
     <div>
-      <span>{props.starRating} stars: </span>
-      <div className='RARProgressBarContainer'>
+      <span onClick={() => {starSort()}}>{props.starRating} stars: </span>
+      {/* <div className='RARProgressBarContainer'>
         <div className='RARProgressBarFiller' style={{width: `${props.ratePercent}%`}}>
           <div>&nbsp;</div>
         </div>
-      </div><br />
+      </div> */}
+      <Slider
+      disabled
+      defaultValue={props.ratePercent}
+      valueLabelDisplay="off"
+      aria-labelledby="breakdownSlider" />
+      <br />
     </div>
   )
 }
